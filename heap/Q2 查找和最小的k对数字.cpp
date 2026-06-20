aced4f7b045f8c8b3fbecb1df7b8daaccc628df8
3Dcodety <202508130435@csust.edu.cn>
2026-06-20 19:52:29 +0800

    Update Q2 查找和最小的k对数字.cpp

--- heap/Q2 查找和最小的k对数字.cpp
+++ heap/Q2 查找和最小的k对数字.cpp
@@ -29,8 +29,49 @@ public:
 而 `emplace` 则是利用 C++11 的完美转发机制，直接接受**构造函数所需的参数**，在容器管理的内存空间中“就地”拼装对象，
 从而省去了中间的拷贝和移动步骤。因此，在需要通过传递多参数来组合成复合类型（如 `pair`、`tuple` 或自定义结构体）的
 场景下，使用 `emplace` 往往比 `push` 更加高效且代码更加简洁。
+
+//自定义结构体
+    class Solution2 {
+private:
+ struct node
+ {
+    int sum,i,j;
+    node(int s,int i,int j):sum(s),i(i),j(j){}
+    bool operator >(const node& other)
+    const {
+                return sum>other.sum;
+          }
+ };
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        vector<vector<int>>res;
+        priority_queue<node,vector<node>,greater<node>>q;
+        for(int i=0;i<min(n1,k);i++)
+        {
+            q.emplace(nums1[i]+nums2[0],i,0);
+        }
+        while(k-->0&&!q.empty())
+        {
+            node x=q.top();
+            int i=x.i;
+            int j=x.j;
+            //auto [sum,i,j]=q.top();用auto一行搞定
+            res.push_back({nums1[i],nums2[j]});
+            q.pop();
+            if(j+1<n2)
+            {
+                q.emplace(nums1[i]+nums2[j+1],i,j+1);
+            }
+        }
+        return res;
+    }
+};
+
+
 //内存超限
-class Solution2 {
+class Solution3 {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int n1=nums1.size();
